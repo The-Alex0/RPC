fd5a13c1efb074a3246b3a7517ae9cc153932a82
Alex {} <102436893+The-Alex0@users.noreply.github.com>
2022-12-04 21:39:48 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const server = express();
+
+server.all('/', (req, res)=>{
+
+    res.send("WAIT 3 MINUTES TO SEE YOUR RPC")
+
+})
+
+function keepAlive(){
+
+    server.listen(process.env.PORT, ()=>{console.log("Alex RPC IS READY")});
+
+}
+
+module.exports = keepAlive
+
